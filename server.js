4fe8ec27432376f497e0ddfebac4d8b1efbe7693
ambikaprasad <akashboard@gmail.com>
2023-06-28 10:13:28 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  fs.readFile("./data/cities.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error retrieving data");
+    } else {
+      res.json(JSON.parse(data));
+    }
+  });
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
